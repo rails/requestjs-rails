ad2ae9eb2a864c785251f97bd8f6874ab0a86e6a
Marcelo Lauxen <marcelolauxen16@gmail.com>
2021-08-11 08:55:47 -0300

    Change importmap

--- lib/requestjs/engine.rb
+++ lib/requestjs/engine.rb
@@ -2,14 +2,14 @@ module Requestjs
   class Engine < ::Rails::Engine
     initializer "requestjs.assets" do
       if Rails.application.config.respond_to?(:assets)
-        Rails.application.config.assets.precompile += %w( request )
+        Rails.application.config.assets.precompile += %w( rails-requestjs )
       end
     end
 
     initializer "requestjs.importmap" do
       if Rails.application.config.respond_to?(:importmap)
         Rails.application.config.importmap.paths.tap do |paths|
-          paths.asset "@rails/request.js", path: "request"
+          paths.asset "@rails/request.js", path: "rails-requestjs"
         end
       end
     end
