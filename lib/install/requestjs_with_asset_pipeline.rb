b5fdb45e78d1707483f71f25eb595d340f9f761a
Dane Wilson <dnkw@hotmail.com>
2021-09-26 15:44:54 -0500

    Fix broken install in Rails 7

--- lib/install/requestjs_with_asset_pipeline.rb
+++ lib/install/requestjs_with_asset_pipeline.rb
@@ -1,4 +1,5 @@
 APP_JS_ROOT = Rails.root.join("app/assets/javascripts")
+APP_JS_PATH = APP_JS_ROOT.exist? ? APP_JS_ROOT : Rails.root.join("app/javascript")
 
-say "Import Request.JS in existing app/assets/javascripts/application.js"
-append_to_file APP_JS_ROOT.join("application.js"), %(import "@rails/request.js"\n)
+say "Import Request.JS in existing #{APP_JS_PATH}"
+append_to_file APP_JS_PATH.join("application.js"), %(import "@rails/request.js"\n)
