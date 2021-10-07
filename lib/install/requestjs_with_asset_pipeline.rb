88e3109334c96157e6be28be7b2f36ae0c95199a
Dane Wilson <dnkw@hotmail.com>
2021-10-07 10:18:42 -0500

    Add check to ensure application.js is always created

--- lib/install/requestjs_with_asset_pipeline.rb
+++ lib/install/requestjs_with_asset_pipeline.rb
@@ -1,5 +1,9 @@
 APP_JS_ROOT = Rails.root.join("app/assets/javascripts")
 APP_JS_PATH = APP_JS_ROOT.exist? ? APP_JS_ROOT : Rails.root.join("app/javascript")
 
+if !APP_JS_PATH.exist?
+  create_file Rails.root.join("app/javascript").join("application.js")
+end
+
 say "Import Request.JS in existing #{APP_JS_PATH}"
 append_to_file APP_JS_PATH.join("application.js"), %(import "@rails/request.js"\n)
