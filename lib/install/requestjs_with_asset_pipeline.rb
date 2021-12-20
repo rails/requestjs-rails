f8298a7382ab7778ff0e4bf088f06ac5d31cb6b7
Dane Wilson <dnwilson@users.noreply.github.com>
2021-12-20 13:30:16 -0500

    Fix broken install in Rails 7 (#3)
    * Fix broken install in Rails 7
    
    * Add check to ensure application.js is always created
    
    * Remove unnecessary join
    
    Signed-off-by: Dane Wilson <dnkw@hotmail.com>
    
    * Clarify logic

--- lib/install/requestjs_with_asset_pipeline.rb
+++ lib/install/requestjs_with_asset_pipeline.rb
@@ -1,4 +1,7 @@
 APP_JS_ROOT = Rails.root.join("app/assets/javascripts")
+APP_JS_PATH = APP_JS_ROOT.exist? ? APP_JS_ROOT : Rails.root.join("app/javascript")
 
-say "Import Request.JS in existing app/assets/javascripts/application.js"
-append_to_file APP_JS_ROOT.join("application.js"), %(import "@rails/request.js"\n)
+create_file Rails.root.join("app/javascript/application.js") unless APP_JS_PATH.exist?
+
+say "Import Request.JS in existing #{APP_JS_PATH}"
+append_to_file APP_JS_PATH.join("application.js"), %(import "@rails/request.js"\n)
