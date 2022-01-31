c6212c44f85bcbcd4313ea2a80bffe59087e5593
Kirill Platonov <kirill@platonov.net>
2022-01-31 09:36:25 +0300

    Precompile requestjs.js

--- lib/requestjs/engine.rb
+++ lib/requestjs/engine.rb
@@ -2,7 +2,7 @@ module Requestjs
   class Engine < ::Rails::Engine
     initializer "requestjs.assets" do
       if Rails.application.config.respond_to?(:assets)
-        Rails.application.config.assets.precompile += %w( rails-requestjs )
+        Rails.application.config.assets.precompile += %w( rails-requestjs requestjs )
       end
     end
 
