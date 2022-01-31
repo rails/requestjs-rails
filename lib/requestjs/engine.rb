ff767c5fc1a320d3673bad0ba63831a91b8961b1
Marcelo Lauxen <marcelolauxen16@gmail.com>
2022-01-31 08:38:54 -0300

    Use extensions of the precompiled files'

--- lib/requestjs/engine.rb
+++ lib/requestjs/engine.rb
@@ -2,7 +2,7 @@ module Requestjs
   class Engine < ::Rails::Engine
     initializer "requestjs.assets" do
       if Rails.application.config.respond_to?(:assets)
-        Rails.application.config.assets.precompile += %w( rails-requestjs requestjs )
+        Rails.application.config.assets.precompile += %w( rails-requestjs.js requestjs.js )
       end
     end
 
